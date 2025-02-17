9fdd382596046291b31a09bf08d7419a7a1b413d
mcorzov <146891924+mcorzov@users.noreply.github.com>
2025-02-17 10:03:23 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+alert ("¡BIENVENIDO!")
+var usuarioRegistrado="mica";
+var contraseñaRegistrada="123";
+
+var usuarioIngresado=prompt("INGRESE EL USUARIO");
+var contraseñaIngresada=prompt("INGRESE LA CONTRASEÑA");
+
+if(usuarioIngresado==usuarioRegistrado && contraseñaIngresada==contraseñaRegistrada){
+    alert ("acceso correcto");
+}else{
+    alert ("acceso denegado");
+}
